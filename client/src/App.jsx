6984951bdc80e1f58aef15f6c74377865bc87dba
Chayutp <135034854+Chayutp@users.noreply.github.com>
2024-01-04 16:27:04 +0700

    feat:test-1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,46 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  return <div className="App">{/* Start coding here */}</div>;
+  const [travel, setTravel] = useState([]);
+
+  const getTravel = async () => {
+    let result = await axios.get(
+      `http://localhost:4001/trips?keywords=${travel}`
+    );
+    setTravel(result.data.data);
+  };
+
+  useEffect(() => {
+    getTravel();
+  }, []);
+
+  return (
+    <div className="App">
+      <div className="Travel-head">
+        <h1 className="Travel-title">เที่ยวไหนดี</h1>
+      </div>
+      <div className="Travel-search">
+        <input placeholder="ค้นหาที่เที่ยว"></input>
+      </div>
+      <div className="Travel-list">
+        {travel.map((item) => {
+          return (
+            <div className="travelmain">
+              <div className="travel-preview">
+                <img src={item.photos[0]} width="350" height="350" />
+              </div>
+              <div className="travel-detail">
+                <h1>{item.title}</h1>
+                <h2>{item.description?.slice(0, 100)}</h2>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
